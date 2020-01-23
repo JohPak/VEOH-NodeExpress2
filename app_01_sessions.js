4065aa7aa2c659abc762178cf9d6ce04c97fb3f1
Johanna Pakkala <johannapakkala@Johanna-MacBook-Pro.local>
2020-01-23 18:23:33 +0200

    aaa

---
+++ app_01_sessions.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+
+let app = express();
+
+//luodaan tyhjä array
+let users = [];
+
+//nuolisyntaksilla käsittelijäfunktio
+// next-parametri tarkoittaa "jatka vielä seuraavaankin kuuntelijaan"
+app.use((req, res, next) => {
+    console.log(`path: ${req.path}`);
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    res.send(`Moro`);
+    //res.write
+    //res.end()
+});
+
+app.use((req, res, next) => {
+    res.status(404);
+    res.send('page not found');
+});
+//app.post()
+ app.listen(PORT);
